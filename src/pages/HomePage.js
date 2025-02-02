d5655a552c29e82e188fe9ee6a1ab27532833380
Tanish Dadarya Gupta <114875082+tanishdadaryagupta123@users.noreply.github.com>
2025-02-02 15:23:38 +0530

    chat

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to BeyondChats</h1>
+      <p>Your intelligent chatbot solution for business</p>
+    </div>
+  );
+};
+
+export default HomePage; 
